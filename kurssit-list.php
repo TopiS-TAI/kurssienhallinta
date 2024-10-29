0d3b2c9816e008809fcdbae761867a9d4ab24e5b
Topi Saavalainen <topi.saavalainen@edu.turku.fi>
2024-10-29 21:34:41 +0200

    Kursseihin korttinäkymä ja kirjautuneet opiskelijat

--- kurssit-list.php
+++ kurssit-list.php
@@ -1,32 +1,41 @@
-<table>
-    <tbody>
-        <thead>
-            <tr>
-                <th>Kurssin nimi</th>
-                <th>Kuvaus</th>
-                <th>Aloituspaiva</th>
-                <th>Lopetuspaiva</th>
-                <th>Opettaja</th>
-                <th>Tila</th>
-            </tr>
-        </thead>
+<section class="kurssit">
 <?php
     include("connect.php");
-    $sql = "SELECT kurssit.nimi, kurssit.kuvaus, kurssit.aloituspaiva, kurssit.lopetuspaiva, opettajat.etunimi, opettajat.sukunimi, tilat.nimi AS tilan_nimi FROM kurssit, opettajat, tilat WHERE kurssit.opettaja = opettajat.id AND kurssit.tila = tilat.id";
+    $sql = "SELECT k.id, k.nimi, k.kuvaus, k.aloituspaiva, k.lopetuspaiva, 
+        oj.etunimi AS oj_etunimi, 
+        oj.sukunimi AS oj_sukunimi,
+        t.nimi AS t_nimi,
+        GROUP_CONCAT(ok.etunimi, ' ', ok.sukunimi) as ok_nimet
+        FROM kurssit k
+        JOIN opettajat oj ON k.opettaja = oj.id
+        JOIN kurssikirjautumiset kk ON kk.kurssi = k.id
+        JOIN opiskelijat ok ON kk.opiskelija = ok.id
+        JOIN tilat t ON k.tila = t.id
+        GROUP BY k.id;";
     $query = $conn->prepare($sql);
     $query->execute();
     $kurssit = $query->fetchAll();
 
     foreach($kurssit as $kurssi) { ?>
-        <tr>
-            <td><?php echo htmlspecialchars($kurssi['nimi']);?></td>
-            <td><?php echo htmlspecialchars($kurssi['kuvaus']);?></td>
-            <td><?php echo date_format(date_create($kurssi['aloituspaiva']), "d.m.Y");?></td>
-            <td><?php echo date_format(date_create($kurssi['lopetuspaiva']), "d.m.Y");?></td>
-            <td><?php echo htmlspecialchars($kurssi['etunimi'] . " " . $kurssi['sukunimi']);?></td>
-            <td><?php echo htmlspecialchars($kurssi['tilan_nimi']);?></td>
-        </tr>
+        <div class="kurssi-info">
+            <h2><?php echo htmlspecialchars($kurssi['nimi']);?></h2>
+            <div class="kurssi-detaljit">
+                <p><?php echo htmlspecialchars($kurssi['oj_etunimi'] . " " . $kurssi['oj_sukunimi']);?></p>
+                <p><?php echo date_format(date_create($kurssi['aloituspaiva']), "d.m.Y");?>–<?php echo date_format(date_create($kurssi['lopetuspaiva']), "d.m.Y");?></p>
+                <p><?php echo htmlspecialchars($kurssi['t_nimi']);?></p>
+            </div>
+            <p class="kurssi-kuvaus"><?php echo htmlspecialchars($kurssi['kuvaus']);?></p>
+            <details class="kurssi-opiskelijat">
+                <summary>Opiskelijat</summary>
+                <ul>
+                <?php 
+                    $opiskelijat = explode(",", $kurssi['ok_nimet']);
+                    foreach ($opiskelijat as $opiskelija) {
+                        echo '<li>' . htmlspecialchars($opiskelija) . '</li>';
+                    }?>
+                </ul>
+            </details>
+        </div>
     <?php }
 ?>
-</tbody>
-    </table>
+</section>
