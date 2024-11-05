9b4b54ecec81a20796f95d2e7eb358e091bd248d
Topi Saavalainen <topi.saavalainen@edu.turku.fi>
2024-11-05 15:06:26 +0200

    Opiskelijoiden viikkoaikataulu

--- opiskelija-info.php
+++ opiskelija-info.php
@@ -1,10 +1,41 @@
 <?php
+    $times = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+    $days = ["ma", "ti", "ke", "to", "pe", "la", "su"];
     $id = $_GET['id'];
     include("connect.php");
-    $sql = "SELECT kk.*, k.nimi, k.aloituspaiva, k.lopetuspaiva FROM kurssit k, kurssikirjautumiset kk WHERE kk.opiskelija = ? AND k.id = kk.kurssi;";
+
+    $sql = "SELECT kk.*, k.nimi, k.aloituspaiva, k.lopetuspaiva 
+        FROM kurssit k, kurssikirjautumiset kk 
+        WHERE kk.opiskelija = ? 
+        AND k.id = kk.kurssi;";
     $query = $conn->prepare($sql);
     $query->execute([$id]);
     $courses = $query->fetchall();
+
+    $sql = "SELECT s.kurssi, s.viikonpaiva, s.aloitusaika, s.lopetusaika, k.nimi, os.etunimi
+        from sessiot s, kurssikirjautumiset kk, kurssit k, opiskelijat os 
+        WHERE s.kurssi = kk.kurssi 
+        AND s.kurssi = k.id 
+        AND kk.opiskelija = os.id 
+        AND os.id = ? 
+        GROUP BY s.id 
+        ORDER BY s.kurssi, s.viikonpaiva;";
+    $query = $conn->prepare($sql);
+    $query->execute([$id]);
+    $sessions = $query->fetchall();
+
+    $sess_list = [];
+    $list = [];
+    foreach ($sessions as $key => $session) {
+        if ($key == 0) {
+            $list = [];
+        } elseif ($sessions[$key]['kurssi'] != $sessions[$key - 1]['kurssi']) {
+            array_push($sess_list, $list);
+            $list = [];
+        }
+        array_push($list, $session);
+    }
+    array_push($sess_list, $list);
 
     $sql = "SELECT * FROM opiskelijat WHERE id = ?";
     $query = $conn->prepare($sql);
@@ -15,7 +46,7 @@
 ?>
 <h2><?php echo "{$id} {$etunimi} {$sukunimi}";?></h2>
 <p>Syntymäaika : <?php echo $student['syntymapaiva']; ?></p>
-<h3>Kurssit:</h3>
+<h3>Kurssit</h3>
 <?php
     if (count($courses)) {
         echo "<ul>";
@@ -33,3 +64,40 @@
         echo "<p>Ei kursseja</p>";
     }
 ?>
+<h3>Aikataulu</h3>
+
+<table class="calendar">
+    <thead>
+        <tr>
+            <th></th>
+            <th colspan="2">Ma</th>
+            <th colspan="2">Ti</th>
+            <th colspan="2">Ke</th>
+            <th colspan="2">To</th>
+            <th colspan="2">Pe</th>
+            <th colspan="2">La</th>
+            <th colspan="2">Su</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach ($times as $time) {
+                echo "<tr><th>" . $time . "</th>";
+                foreach ($days as $i => $day) {
+                    foreach ($sess_list as $sess) {
+                        echo "<td>";
+                        foreach ($sess as $session) {
+                            if ($session['viikonpaiva'] == $day and $session['aloitusaika'] <= $time and $session['lopetusaika'] >= $time) {
+                                echo $session['nimi'];
+                            }
+                        }
+                        echo "</td>";
+                    }
+                }
+                echo "</tr>";
+            }
+        ?>
+    </tbody>
+</table>
+
+
